6d5227fad3dba610fa6fb2f9b91aa2c02ccfb5e6
Pedro Maroco <pedro.maroco@outsystems.com>
2019-02-06 12:58:25 +0000

    feat: removes opening database log

--- src/android/io/sqlc/SQLitePlugin.java
+++ src/android/io/sqlc/SQLitePlugin.java
@@ -377,7 +377,6 @@ public class SQLitePlugin extends CordovaPlugin {
 
         public void run() {
             try {
-                logger.logWarning("Opening database (" + this.dbkey.hashCode() + ")", "SQLite");
                 this.mydb = openDatabase(dbname, this.dbkey, this.openCbc, false);
             } catch (Exception e) {
                 logger.logError("Error opening database: " + e.getMessage(), "SQLite");
